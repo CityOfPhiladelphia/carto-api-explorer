87b90756e764c7047ba067792bea1dbb077412ff
timwis <tim@timwis.com>
2017-02-16 07:38:23 -0500

    add intro copy

--- src/app.js
+++ src/app.js
@@ -7,20 +7,35 @@ import Header from './header'
 @observer
 export default class App extends React.Component {
   render () {
-    const selectedFieldIndex = this.props.store.selectedFieldIndex
-    const selectedField = selectedFieldIndex ? this.props.store.fields[selectedFieldIndex] : null
+    const { domain, table, selectedFieldIndex, fields } = this.props.store
+    const selectedField = selectedFieldIndex ? fields[selectedFieldIndex] : null
 
     return (
       <div className='site' id='page'>
         <Header />
         <article>
           <div className='column row'>
-            <h1 className='contrast'>{this.props.store.table}</h1>
+            <h1 className='contrast'>{table}</h1>
+
+            <p>
+              This dataset is published to <a href='https://carto.com'>carto</a>, which allows you to
+              query the data using SQL (specifically the PostgreSQL flavor with the PostGIS extension).
+              Rather than executing the queries in a database program, you execute them through HTTP
+              (ie. the web browser) via Carto's <a href='https://carto.com/docs/carto-engine/sql-api'>SQL API</a>,
+              passing your query as the <code>q</code> parameter. For example:
+            </p>
+
+            <p><code>https://{domain}/api/v2/sql?q=SELECT * FROM {table}</code></p>
+
+            <p>
+              You can do just about everything you'd expect from a PostgreSQL+PostGIS database - even
+              joins between tables.
+            </p>
 
             <div className='row'>
               <div className='medium-6 columns'>
                 <ul className='tabs vertical' data-tabs>
-                  {this.props.store.fields.map(this.renderFieldTab, this)}
+                  {fields.map(this.renderFieldTab, this)}
                 </ul>
               </div>
               <div className='medium-18 columns'>
